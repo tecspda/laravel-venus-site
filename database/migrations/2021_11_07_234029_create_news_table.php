c81041aa624a25e6053d04bc829e9e85f4c0dd66
tecspda <sdurmanov@gmail.com>
2021-11-09 10:29:22 +0300

    Migration code

---
+++ database/migrations/2021_11_07_234029_create_news_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->string('body', 3000);
+            $table->string('author', 30);
+            $table->date('date');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
